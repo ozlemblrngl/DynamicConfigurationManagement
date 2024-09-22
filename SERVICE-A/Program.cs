125433431c454b3eed5d763ae0ee13c581e773e4
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-22 10:06:26 +0300

    getValue implemented changed

--- SERVICE-A/Program.cs
+++ SERVICE-A/Program.cs
@@ -5,7 +5,7 @@ internal class Program
 	private static void Main(string[] args)
 	{
 		ConfigurationReader configurationReader = new ConfigurationReader("SERVICE-A", "Server=DESKTOP-N9UK4PN;Database=ConfigurationDB;Trusted_Connection=True;TrustServerCertificate=True;", 100000);
-		var result = configurationReader.GetValue<int>("MaxItemCount");
+		var result = configurationReader.GetValue<string>("SiteName");
 		Console.WriteLine(result);
 
 	}
