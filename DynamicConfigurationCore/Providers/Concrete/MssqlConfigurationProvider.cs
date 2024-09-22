4443b05548bb42b777e41fa15bc1755b57d8bcd5
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-22 10:05:22 +0300

    functon rearranged

--- DynamicConfigurationCore/Providers/Concrete/MssqlConfigurationProvider.cs
+++ DynamicConfigurationCore/Providers/Concrete/MssqlConfigurationProvider.cs
@@ -4,16 +4,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DynamicConfigurationCore.Providers.Concrete
 {
-    public class MssqlConfigurationProvider : IConfigProvider
-    {
-        public IEnumerable<Configuration> LoadConfigurations(string applicationName, string connectionString)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<DynamicConfigurationDbContext>();
-            optionsBuilder.UseSqlServer(connectionString);
-            using (var dbContext = new DynamicConfigurationDbContext(optionsBuilder.Options))
-            {
-                return dbContext.Configurations.ToList();
-            }
-        }
-    }
+	public class MssqlConfigurationProvider : IConfigProvider
+	{
+		public IEnumerable<Configuration> LoadConfigurations(string applicationName, string connectionString)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<DynamicConfigurationDbContext>();
+			optionsBuilder.UseSqlServer(connectionString);
+			using (var dbContext = new DynamicConfigurationDbContext(optionsBuilder.Options))
+			{
+				return dbContext.Configurations.Where(c => c.IsActive && c.ApplicationName == applicationName).ToList();
+			}
+		}
+	}
 }
--- DynamicConfigurationCore/Services/Concrete/ConfigurationReader.cs
+++ DynamicConfigurationCore/Services/Concrete/ConfigurationReader.cs
@@ -5,69 +5,68 @@ using Entities;
 
 namespace DynamicConfigurationCore.Services.Concrete
 {
-    public class ConfigurationReader : IConfigurationReader
-    {
-        private readonly string _applicationName;
-        private readonly string _connectionString;
-        private readonly int _refreshTimerIntervalInMs;
-        private readonly IConfigProvider _provider;
-        private System.Timers.Timer _refreshTimer;
-        private Dictionary<string, Configuration> _configurations;
+	public class ConfigurationReader : IConfigurationReader
+	{
+		private readonly string _applicationName;
+		private readonly string _connectionString;
+		private readonly int _refreshTimerIntervalInMs;
+		private readonly IConfigProvider _provider;
+		private System.Timers.Timer _refreshTimer;
+		private Dictionary<string, Configuration> _configurations;
 
-        public ConfigurationReader(string applicationName, string connectionString, int refreshTimerIntervalInMs)
-        {
-            _applicationName = applicationName;
-            _connectionString = connectionString;
-            _refreshTimerIntervalInMs = refreshTimerIntervalInMs;
-            _provider = new MssqlConfigurationProvider();
-            _configurations = new Dictionary<string, Configuration>();
+		public ConfigurationReader(string applicationName, string connectionString, int refreshTimerIntervalInMs)
+		{
+			_applicationName = applicationName;
+			_connectionString = connectionString;
+			_refreshTimerIntervalInMs = refreshTimerIntervalInMs;
+			_provider = new MssqlConfigurationProvider();
+			_configurations = new Dictionary<string, Configuration>();
 
-            LoadConfigurations();
-            StartRefreshTimer();
+			LoadConfigurations();
+			StartRefreshTimer();
 
-        }
+		}
 
-        private void LoadConfigurations()
-        {
+		private void LoadConfigurations()
+		{
 
-            var configurations = _provider.LoadConfigurations(_applicationName, _connectionString)
-                                           .Where(c => c.IsActive);
+			var configurations = _provider.LoadConfigurations(_applicationName, _connectionString);
 
-            _configurations = configurations.ToDictionary(c => c.Name, c => c);
-        }
+			_configurations = configurations.ToDictionary(c => c.Name, c => c);
+		}
 
-        private void StartRefreshTimer()
-        {
-            _refreshTimer = new System.Timers.Timer(_refreshTimerIntervalInMs);
-            _refreshTimer.Elapsed += (sender, args) => LoadConfigurations();
-            _refreshTimer.Start();
-        }
+		private void StartRefreshTimer()
+		{
+			_refreshTimer = new System.Timers.Timer(_refreshTimerIntervalInMs);
+			_refreshTimer.Elapsed += (sender, args) => LoadConfigurations();
+			_refreshTimer.Start();
+		}
 
-        public T GetValue<T>(string key)
-        {
-            if (_configurations.ContainsKey(key))
-            {
-                var configItem = _configurations[key];
+		public T GetValue<T>(string key)
+		{
+			if (_configurations.ContainsKey(key))
+			{
+				var configItem = _configurations[key];
 
-                if (typeof(T) == typeof(int) && int.TryParse(configItem.Value, out var intValue))
-                {
-                    return (T)(object)intValue;
-                }
+				if (typeof(T) == typeof(int) && int.TryParse(configItem.Value, out var intValue))
+				{
+					return (T)(object)intValue;
+				}
 
-                if (typeof(T) == typeof(bool) && bool.TryParse(configItem.Value, out var boolValue))
-                {
-                    return (T)(object)boolValue;
-                }
+				if (typeof(T) == typeof(bool) && bool.TryParse(configItem.Value, out var boolValue))
+				{
+					return (T)(object)boolValue;
+				}
 
-                if (typeof(T) == typeof(double) && double.TryParse(configItem.Value, out var doubleValue))
-                {
-                    return (T)(object)doubleValue;
-                }
+				if (typeof(T) == typeof(double) && double.TryParse(configItem.Value, out var doubleValue))
+				{
+					return (T)(object)doubleValue;
+				}
 
-                return (T)(object)configItem.Value;
-            }
+				return (T)(object)configItem.Value;
+			}
 
-            throw new KeyNotFoundException($"Key '{key}' not found in configuration.");
-        }
-    }
+			throw new KeyNotFoundException($"Key '{key}' not found in configuration.");
+		}
+	}
 }
