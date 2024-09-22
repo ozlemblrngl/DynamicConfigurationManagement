f00a30591b5a6427b7da7c0cbb502b8f41095b16
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-22 09:48:03 +0300

    syntax corrected

--- SERVICE-A/Program.cs
+++ SERVICE-A/Program.cs
@@ -2,11 +2,11 @@
 
 internal class Program
 {
-    private static void Main(string[] args)
-    {
-        ConfigurationReader configurationReader = new ConfigurationReader("SERVICE-A", "Server=DESKTOP-N9UK4PN;Database=ConfigurationDB;Trusted_Connection=True;TrustServerCertificate=True;", 100000);
-        var result = configurationReader.GetValue<string>("siteName");
-        Console.WriteLine(result);
+	private static void Main(string[] args)
+	{
+		ConfigurationReader configurationReader = new ConfigurationReader("SERVICE-A", "Server=DESKTOP-N9UK4PN;Database=ConfigurationDB;Trusted_Connection=True;TrustServerCertificate=True;", 100000);
+		var result = configurationReader.GetValue<string>("SiteName");
+		Console.WriteLine(result);
 
-    }
+	}
 }
