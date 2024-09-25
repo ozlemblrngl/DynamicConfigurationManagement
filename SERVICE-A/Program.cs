2518125269430ce358f1cc8732193a0e9f87552c
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2024-09-25 09:30:42 +0300

    programcs of service-a syntax changed

--- SERVICE-A/Program.cs
+++ SERVICE-A/Program.cs
@@ -4,11 +4,15 @@ internal class Program
 {
 	private static void Main(string[] args)
 	{
-		ConfigurationReader configurationReader = new ConfigurationReader("SERVICE-A", "Server=DESKTOP-N9UK4PN;Database=ConfigurationDB;Trusted_Connection=True;TrustServerCertificate=True;", 100000);
-		var result = configurationReader.GetValue("SiteName");
-		Console.WriteLine("For GetValue : " + result);
+		ConfigurationReader configurationReader = new ConfigurationReader("SERVICE-A", "Server=DESKTOP-N9UK4PN;Database=ConfigurationDB;Trusted_Connection=True;TrustServerCertificate=True;", 15000);
+		while (true)
+		{
+			var result = configurationReader.GetValue("SiteName");
+			Console.WriteLine("For GetValue :" + result);
 
-		var resultT = configurationReader.GetValue<String>("SiteName");
-		Console.WriteLine("For GetValueT :" + resultT);
+			var resultT = configurationReader.GetValue<String>("SiteName");
+			Console.WriteLine("For GetValueT :" + resultT);
+			Thread.Sleep(10000);
+		}
 	}
 }
